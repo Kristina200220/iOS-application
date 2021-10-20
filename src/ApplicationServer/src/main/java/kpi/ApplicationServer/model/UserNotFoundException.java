beda3f65b1ed4117d1b087c74a9aaefb1470d3c7
Greg <realgregorygrytsyniak@gmail.com>
2021-10-20 12:38:12 +0300

    Added application_server 0.1

---
+++ src/ApplicationServer/src/main/java/kpi/ApplicationServer/model/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package kpi.ApplicationServer.model;
+
+class UserNotFoundException extends RuntimeException {
+
+    UserNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
---
+++ src/ApplicationServer/src/main/java/kpi/ApplicationServer/model/UserRepository.java
@@ -0,0 +1,7 @@
+package kpi.ApplicationServer.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
